61991c846d4244c8cd83fe35a99feb44a778ef47
Yibei Han <hanyibei@hyb-MacBook-Pro.local>
2024-05-12 01:32:44 -0400

    change to affine

--- mapping/create_cat_video.py
+++ mapping/create_cat_video.py
@@ -24,9 +24,9 @@ class CreateCatVideo:
         # print(self.width)
 
         self.body_parts = [
-            "head", "body", 
+            "head", "body",
             "upper_left_arm", "lower_left_arm",
-            "upper_right_arm", "lower_right_arm", 
+            "upper_right_arm", "lower_right_arm",
             "left_leg", "right_leg"
         ]
         # self.body_parts = [
@@ -34,10 +34,10 @@ class CreateCatVideo:
         # ]
         self.all_frames_dst_pts = {}     # {"head": [[frame_1], frame_2, ...], "body": [[frame_1], frame_2, ...], "upper_left_arm": [...]}
 
-    # # dst pnts base on square little man 
+    # # dst pnts base on square little man
     def load_features_dst_points_all_frames(self):
         for part in self.body_parts:
-            csv_file_path = f'./result/dst_points/{part}.csv' 
+            csv_file_path = f'./result/dst_points/{part}.csv'
             df = pd.read_csv(csv_file_path)
 
             # DataFrame `df` has columns named 'x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3', 'x_4', 'y_4'
@@ -55,9 +55,9 @@ class CreateCatVideo:
                 ]
                 # dst_points = [
                 #     [row[f'{part}_1_x'] * self.width, row[f'{part}_1_y'] * self.height],
-                #     [row[f'{part}_2_x'] * self.width, row[f'{part}_2_y'] * self.height],  
+                #     [row[f'{part}_2_x'] * self.width, row[f'{part}_2_y'] * self.height],
                 #     [row[f'{part}_3_x'] * self.width, row[f'{part}_3_y'] * self.height],
-                #     [row[f'{part}_4_x'] * self.width, row[f'{part}_4_y'] * self.height],  
+                #     [row[f'{part}_4_x'] * self.width, row[f'{part}_4_y'] * self.height],
                 # ]
                 dst_points = np.array(dst_points, dtype=np.float32)
                 self.all_frames_dst_pts[part].append(dst_points)
@@ -67,7 +67,7 @@ class CreateCatVideo:
         for part in self.body_parts:
             dst_points_dict[part] = self.all_frames_dst_pts[part][frame_num]
         return dst_points_dict
-    
+
     def create_dst_points_dict_original_man(self, frame_num):
         file_path = f'./result/human_features_diff_frames/human_features_{frame_num}.csv'  # Update this to the path of your CSV file
         df = pd.read_csv(file_path)
@@ -98,10 +98,10 @@ class CreateCatVideo:
         catImage = CreateCatImage(dst_points_dict)
         output_image = catImage.get_cat_image()
         return output_image
-    
+
 
     def create_video_from_frames_original_man(self, output_video_path, fps=10, frame_size=(1710, 1080)):   # frame_size = (width, height)
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')  
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
 
         selected_frames = [1, 4, 10, 16]
@@ -128,20 +128,20 @@ class CreateCatVideo:
             for frame_num in selected_frames:
                 frame = hashmap[frame_num]
                 out.write(frame)
-        
+
         out.release()
         print("The video was successfully created.")
 
 
     def create_video_from_frames_square_man(self, output_video_path, fps=10, frame_size=(1710, 1080)):   # frame_size = (width, height)
         self.load_features_dst_points_all_frames()  # dst pnts base on square little man only
-        
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')  
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
 
-        video_total_frame_num = len(self.all_frames_dst_pts['head'])
+        # video_total_frame_num = len(self.all_frames_dst_pts['head'])
         # print(video_total_frame_num)  # 2509
-        video_total_frame_num = 60     # comment out this line if want to create the full-length video
+        video_total_frame_num = 30     # comment out this line if want to create the full-length video
 
         for frame_num in range(video_total_frame_num):  # Loop through each frame
             frame = self.one_frame_cat(frame_num, 'square_man')
@@ -153,15 +153,14 @@ class CreateCatVideo:
                 out.write(frame)  # Write the frame into the video
             else:
                 print(f"Warning: Could not create frame")
-        
+
         out.release()
         print("The video was successfully created.")
 
 
-
 if __name__ == "__main__":
-    dst_type = 'original_man'
-    # dst_type = 'square_man'
+    # dst_type = 'original_man'
+    dst_type = 'square_man'
 
     catVideo = CreateCatVideo()
     frame_size = (catVideo.width, catVideo.height)
@@ -172,7 +171,7 @@ if __name__ == "__main__":
     elif dst_type == 'square_man':
         output_video_path = './result/cat_video_square.mp4'
         catVideo.create_video_from_frames_square_man(output_video_path, fps=6, frame_size=frame_size)
-    
+
     # # Display the final output
     # cv2.imshow('Warped Image on Canvas', output_image)
     # cv2.waitKey(0)
